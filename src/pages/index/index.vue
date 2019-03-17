91c9e507aa88abc7de1651cf0f0c466ebd72f605
陈慕柏 <18300193434@163.com>
2019-03-17 21:30:21 +0800

    Update index.vue
    轮播图完成

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,19 +1,78 @@
 <template>
- 
   <view>
-    首页
+    <!-- 搜索框开始 -->
+    <navigator url="../search/main" hover-class="none" class="search">
+      <view class="search-input">
+        <icon type="search" size="32rpx" class="search-icon"></icon>搜索
+      </view>
+    </navigator>
+    <!-- 搜索框结束 -->
+    <!-- 轮播图开始 -->
+    <swiper
+      indicator-dots
+      autoplay
+      circular
+      interval="2000"
+      indicator-color="rgba(255,255,255,0.3)"
+      indicator-active-color="rgba(255,255,255,1)"
+    >
+      <block v-for="(item,index) in imgUrls" :key="index">
+        <swiper-item>
+          <image :src="item.image_src" class="slide-image" mode="aspectfill"></image>
+        </swiper-item>
+      </block>
+    </swiper>
+    <!-- 轮播图结束 -->
   </view>
 </template>
 
 <script>
 export default {
-  data(){
-    
+  data() {
+    return{
+      imgUrls:[],
+      cate:[1,2,3,4]
+    }
+  },
+  // 小程序加载的时候触发
+  onLoad(){
+    // 请求数据
+    wx.request({
+      url: 'https://www.zhengzhicheng.cn/api/public/v1/home/swiperdata', //开发者服务器接口地址",
+      method: 'GET',
+      success: res => {
+        this.imgUrls = res.data.message;
+      },
+    });
   }
-  
-}
+};
 </script>
 
 <style>
-
+/* 头部搜索 */
+.search {
+  padding: 20rpx;
+  background-color: #eb4450;
+}
+.search-input {
+  height: 60rpx;
+  background: #fff;
+  border-radius: 12rpx;
+  color: #333;
+  font-size: 30rpx;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.search-icon {
+  margin-right: 16rpx;
+}
+/* 轮播图 */
+swiper{
+  height: 340rpx;
+}
+.slide-image{
+  width: 750rpx;
+  height: 340rpx;
+}
 </style>
