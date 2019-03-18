0e6ffc94512509c2595971f361d7d0564257dd97
陈慕柏 <18300193434@163.com>
2019-03-18 18:56:32 +0800

    封装请求方法
    使用promise封装请求

--- src/pages/categories/index.vue
+++ src/pages/categories/index.vue
@@ -1,23 +1,130 @@
 <template>
- 
   <view>
+    <!-- 搜索组件开始 -->
     <Search></Search>
+    <!-- 搜索组件结束 -->
+    <!-- 分类内容开始 -->
+    <view>
+      <view class="cata">
+        <!-- 分类左边开始 -->
+        <scroll-view scroll-y class="cata-left">
+          <block v-for="(item,index) in [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,18,19,20,22,2222,222,22,2,2,2,2]" :key="index"
+            >
+          <view class="item" :class="{ active:index ===tabIndex}" @tap="changTabs(index)" 
+          >大家电</view>
+          </block>
+        </scroll-view>
+
+        <!-- 分类左边结束 -->
+        <!-- 分类右边开始 -->
+         <!-- 4.0 分类右边 -->
+      <scroll-view scroll-y class="cata-right">
+        <view class="cata-right-title">
+          电视
+        </view>
+        <view class="cate-right-list">
+          <block v-for="(item,index) in [1,2,2,3,3,1,1,1,1,1,2,2,3,3,1,1,1,1,1,2,2,3,3,1,1,1,1,1,1,1]" :key="index">
+            <view class="cate-right-list-item">
+              <image src="https://img.alicdn.com/imgextra/i1/2536908852/TB2PZ9rpstnpuFjSZFKXXalFFXa_!!2536908852-0-beehive-scenes.jpg_360x360xzq90.jpg_.webp">
+              </image>
+              <view>分类名称</view>
+            </view>
+          </block>
+        </view>
+      </scroll-view>
+        <!-- 分类右边结束 -->
+      </view>
+    </view>
+    <!-- 分类内容结束 -->
   </view>
 </template>
 
 <script>
 import Search from "../../components/Search.vue";
+import request from "../../utils/request.js"
 export default {
-  data(){
-    
+  data() {
+    return {
+      tabIndex :0
+    }
   },
   components: {
     Search
+  },
+  onLoad(){
+    request("https://www.zhengzhicheng.cn/api/public/v1/categories").then((res)=>{
+      console.log(res);
+    })
+  },
+  methods:{
+    changTabs(index){
+      this.tabIndex = index;
+    }
   }
-  
-}
+};
 </script>
 
 <style>
-
+.cata{
+  display: flex;
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 100rpx;
+  bottom: 0;
+}
+.cata-left{
+  width: 200rpx;
+  /* background-color: blue; */
+  height: 100%;
+  flex-shrink: 0;
+}
+.cata-left .item {
+  line-height: 100rpx;
+  text-align: center;
+  border-bottom: 1rpx solid #999;
+  position: relative;
+}
+.cata-left .item.active{
+  background-color: #fff;
+  color:red;
+}
+.cata-left .item.active::before{
+  content: "";
+  position: absolute;
+  
+  width: 4rpx;
+  background-color: red;
+  left: 0;
+  top:10rpx;
+  bottom:10rpx;
+}
+.cata-right{
+  flex: 1;
+  height: 100%;
+}
+.cata-right-title{
+  text-align: center;
+  padding: 40rpx 0;
+}
+.cata-right-title::after,
+.cata-right-title::before{
+  content: "/";
+  color:#ccc;
+  margin: 0 20rpx;
+}
+.cate-right-list{
+  display: flex;
+  flex-wrap: wrap;
+}
+.cate-right-list-item{
+  width: 33.33%;
+  font-size: 30rpx;
+  text-align: center;
+  padding: 20rpx 0;
+}
+.cate-right-list-item image{
+  width: 120rpx;
+  height: 120rpx;
+}
 </style>
---
+++ src/utils/request.js
@@ -0,0 +1,19 @@
+// 粉装小程序请求的方法
+
+function request(url, data = {}, header = {}, method = "GET") {
+    return new Promise((resolve, reject) => {
+        wx.request({
+            url, //开发者服务器接口地址",
+            data, //请求的参数",
+            method,
+            header,
+            success: res => {
+                resolve(res)
+            },
+            faile: () => {
+                reject()
+            },
+        });
+    })
+}
+export default request;
